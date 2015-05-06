89f363eb16ff944dd39ece390f0be0bd6b5759e8
Johan <contact@johansatge.fr>
2015-05-06 17:37:49 +0200

    Sass structure & UI progress

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -3,6 +3,8 @@ var Tray = require('tray');
 var BrowserWindow = require('browser-window');
 var fs = require('fs');
 var Menu = require('menu');
+var ipc = require('ipc');
+var dialog = require('dialog');
 
 require('crash-reporter').start();
 
@@ -32,10 +34,7 @@ app.on('ready', function()
             },
             {
                 label: 'Open settings panel',
-                click: function()
-                {
-                    _toggleMainWindow();
-                }
+                click: _toggleSettingsWindow
             },
             {
                 type: 'separator'
@@ -55,11 +54,52 @@ app.on('ready', function()
         ]
     ));
 
-    var _toggleMainWindow = function()
+    ipc.on('settings-ready', function(event)
+    {
+        var sample_settings = [ // @todo get real ones (localStorage ? User file ?)
+            {
+                url: 'http://test-url-1',
+                dir: '/Users/coucou1',
+                options: '--size 5'
+            },
+            {
+                url: 'http://test-url-2',
+                dir: '/Users/coucou2',
+                options: '--size 5'
+            },
+            {
+                url: 'http://test-url-3',
+                dir: '/Users/coucou3',
+                options: '--size 5'
+            },
+            {
+                url: 'http://test-url-4',
+                dir: '/Users/coucou4',
+                options: '--size 5'
+            }
+        ];
+        event.sender.send('current-settings', sample_settings);
+    });
+
+    ipc.on('select-directory', function(event, task_id)
+    {
+        dialog.showOpenDialog(mainWindow, {title: '@todo title', properties: ['openDirectory']}, function(paths)
+        {
+            if (typeof paths !== 'undefined')
+            {
+                event.sender.send('directory-selected', {task_id: task_id, path: paths[0]});
+            }
+        });
+    });
+
+    /**
+     * Creates the settings window
+     */
+    var _toggleSettingsWindow = function()
     {
         if (mainWindow === null)
         {
-            mainWindow = new BrowserWindow({width: 800, height: 600, show: false});
+            mainWindow = new BrowserWindow({width: 800, height: 600, show: false, frame: false, transparent: true});
             mainWindow.webContents.on('did-finish-load', function()
             {
                 mainWindow.show();
@@ -68,7 +108,16 @@ app.on('ready', function()
             mainWindow.webContents.loadUrl('file://' + mainPath + '/assets/html/settings.html');
             mainWindow.on('closed', function()
             {
+                // @todo do not quit on close
                 mainWindow = null;
+            });
+            mainWindow.on('focus', function()
+            {
+                mainWindow.webContents.send('window-focus');
+            });
+            mainWindow.on('blur', function()
+            {
+                mainWindow.webContents.send('window-blur');
             });
         }
         else
@@ -77,21 +126,5 @@ app.on('ready', function()
         }
     };
 
-    /*
-    var ipc = require('ipc');
-    ipc.on('asynchronous-message', function(event, arg)
-    {
-        console.log(arg);  // prints "ping"
-        event.sender.send('asynchronous-reply', 'pong');
-    });
-
-    ipc.on('synchronous-message', function(event, arg)
-    {
-        console.log(arg);  // prints "ping"
-        event.returnValue = 'pong';
-    });
-    */
-
-    _toggleMainWindow();
-
+    _toggleSettingsWindow();
 });
--- app/assets/js/settings.js
+++ app/assets/js/settings.js
@@ -4,22 +4,121 @@
     'use strict';
 
     var module = {};
+    var taskTemplate = null;
+    var tasksList = null;
+    var ipc = require('ipc');
 
     module.init = function()
     {
-        console.log('settings init @todo');
-
-
-
-
-
-        /*var ipc = require('ipc');
-
-        ipc.on('asynchronous-reply', function(arg)
+        taskTemplate = document.querySelector('.js-task-template');
+        tasksList = document.querySelector('.js-tasks-list');
+        document.querySelector('.js-add-task').addEventListener('click', function(evt)
         {
-            console.log(arg); // prints "pong"
+            evt.preventDefault();
+            _createTask({}, true);
         });
-        ipc.send('asynchronous-message', 'ping');*/
+        ipc.on('current-settings', _onInitTasks);
+        ipc.send('settings-ready');
+        ipc.on('directory-selected', _onSelectedDirectory);
+        ipc.on('window-focus', function()
+        {
+            document.body.className = document.body.className.replace('js-blur', '');
+        });
+        ipc.on('window-blur', function()
+        {
+            document.body.className += ' js-blur';
+        });
+    };
+
+    /**
+     * Inits available tasks when received by the app
+     * @param tasks
+     */
+    var _onInitTasks = function(tasks)
+    {
+        for (var index = 0; index < tasks.length; index += 1)
+        {
+            _createTask(tasks[index], false);
+        }
+    };
+
+    /**
+     * Appends a new task to the list
+     * @param data
+     * @param opened
+     */
+    var _createTask = function(data, opened)
+    {
+        var new_task = document.createElement('div');
+        new_task.innerHTML = taskTemplate.innerHTML;
+        new_task.className = taskTemplate.getAttribute('rel');
+        tasksList.insertBefore(new_task, tasksList.firstChild);
+        for (var property in data)
+        {
+            var node = new_task.querySelector('.js-' + property);
+            node.setAttribute('value', data[property]);
+        }
+        new_task.querySelector('.js-accordion').addEventListener('click', _onToggleTask);
+        new_task.querySelector('.js-dir').addEventListener('change', _updateTasks);
+        new_task.querySelector('.js-dir-select').addEventListener('click', _onSelectDirectory);
+        if (opened)
+        {
+            new_task.className += ' js-opened';
+        }
+        new_task.setAttribute('id', 'task-' + new Date().getTime() + '-' + document.querySelectorAll('.js-task').length);
+        _updateTasks();
+    };
+
+    /**
+     * Toggles a task
+     * @param evt
+     */
+    var _onToggleTask = function(evt)
+    {
+        var task = evt.currentTarget.parentNode.parentNode;
+        if (task.className.search('js-opened') !== -1)
+        {
+            task.className = task.className.replace('js-opened', '');
+        }
+        else
+        {
+            task.className += ' js-opened';
+        }
+    };
+
+    /**
+     * Selects a directory by using the native file dialog
+     * @param evt
+     */
+    var _onSelectDirectory = function(evt)
+    {
+        evt.preventDefault();
+        ipc.send('select-directory', evt.currentTarget.parentNode.parentNode.parentNode.parentNode.parentNode.getAttribute('id'));
+    };
+
+    /**
+     * Updates UI when a directory has been selected
+     * @param data
+     */
+    var _onSelectedDirectory = function(data)
+    {
+        var input = document.querySelector('#' + data.task_id + ' .js-dir');
+        input.value = data.path;
+        input.dispatchEvent(new Event('change'));
+    };
+
+    /**
+     * Updates task blocks
+     */
+    var _updateTasks = function()
+    {
+        var tasks = document.querySelectorAll('.js-task');
+        for (var index = 0; index < tasks.length; index += 1)
+        {
+            var task = tasks[index];
+            var title = task.querySelector('.js-dir').value;
+            task.querySelector('.js-title').innerHTML = title.length > 0 ? title : 'Unnamed task';
+        }
     };
 
     window.Settings = module;
